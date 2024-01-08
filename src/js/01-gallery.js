5cd51d14728f02b50b5975a047fdb8b933a29557
kuciapakrystian <kuciapakrystian@outlook.com>
2024-01-08 19:23:58 +0100

    final version

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,20 +1,27 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+// Add imports above this line
 import { galleryItems } from './gallery-items';
+// Change code below this line
 
-const gallery = document.querySelector('div.gallery');
-const createGallery = galleryItems // gallery
-  .map(
-    (item) =>
-      `<a class="gallery__item" href="${item.original}">
-  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
-</a>`
-  )
-  .join('');
-gallery.insertAdjacentHTML('beforeend', createGallery); // Create gallery
-const lightbox = new SimpleLightbox('.gallery a', {
-  captionsData: 'alt',
+const imagesMenuEl = document.querySelector('.gallery');
+imagesMenuEl.style.listStyle = 'none';
+
+function createImages(image) {
+  return image
+    .map(
+      ({ preview, original, description }) => `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>
+</li>`
+    )
+    .join('');
+}
+
+imagesMenuEl.insertAdjacentHTML('beforeend', createImages(galleryItems));
+
+const lightbox = new SimpleLightbox('.gallery__link', {
   captionDelay: 250,
-  captionPosition: 'bottom',
+  captionsData: 'alt',
 });
-console.log(galleryItems);
